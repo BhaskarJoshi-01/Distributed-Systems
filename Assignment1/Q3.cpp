cbb492c1e7a156529adc0856f0c7acf3890c1ff4
BhaskarJoshi-01 <bhaskar.joshi@research.iiit.ac.in>
2022-01-25 19:41:06 +0530

    taken input for q3

--- Assignment1/Q3.cpp
+++ Assignment1/Q3.cpp
@@ -9,10 +9,13 @@
 // eq_mat is the A_matrix
 // val_mat is the b_matrix
 // divs is no_of_coefficients_per_process
-// displs is begining_index_of_coeff_of_each_process
+// displs is displacement_vector
 // rpp is rows_per_process
 // np0 is number_of_process_with_rows=rpp
 // np0 is number_of_process_with_rows=rpp+1
+// var_perm is array to store temp_col_num
+// rows_before is actual_posn in 1d view
+// eff_row is proc_assign
 
 int main(int argc, char **argv)
 {
@@ -40,18 +43,71 @@ int main(int argc, char **argv)
     // converting n*n A matrix that was input to 1*n matrix
 
     float eq_mat[oned_size], val_mat[num_eq];
-    ll divs[num_proc], displs[num_proc];
     // sending no. of equations to every process
     MPI_Bcast(&num_eq, 1, MPI_INT, root_rank, comm);
-    
-    //Defining variables for dividing processes
-    ll rpp,np0,np1;
+
+    // Defining variables for dividing processes
+    ll rpp, rows_per_proc, np0, np1;
+    ll var_perm[num_eq]; // stores the column no. in array
+    for (ll i = 0; i < num_eq; ++i)
+        var_perm[i] = i;
     rpp = num_eq / num_proc; // no of process that shoud be divided into processes
-    // like 12/4=3 
+    // like 12/4=3
 
-    np0 = num_proc - (num_eq % num_proc); // Number of processes with rows = rpp (np0),
+    // now basically what i m trying to do is pair conditions like
+    // 15/4 so there will be like 3 processes with one more row and only
+    // one process with 15/4
+
+    ll rem_proc = num_eq % num_proc;
+    np0 = num_proc - rem_proc;
     np1 = num_proc - np0;
+    // if process rank is less than np1 so we should give it one more pocess
+    rows_per_proc = ((id < np1) ? (1 + rpp) : rpp);
+    ll divs[num_proc], displs[num_proc];
+    memset(divs, 0, sizeof(divs));
+    if (id = root_rank)
+    {
+        // now what i m trying to do is calc the final posn of row in linearized array
+        memset(divs, 0, sizeof(divs));
+        ll rows_before, eff_row;
+        ll i = 0;
+        displs[0] = 0; // used later for disp vector
 
+        while (i < num_eq)
+        {
+            ll k = 0;
+            rows_before = 0;
+            if ((i % num_proc) > np1)
+            {
+                rows_before = np1 * (1 + rpp);
+                rows_before = rpp * (i % num_proc) - rpp * np1 + rows_before;
+            }
+            else
+                rows_before = (i % num_proc) * (1 + rpp);
+            eff_row = divs[i % num_proc] + rows_before;
+            divs[i % num_proc]++;
+            // now we have assigned row in process , now we just read and allocate from imput
+            while (k < num_eq)
+            {
+                fscanf(inputfp, "%f", &eq_mat[k + num_eq * eff_row]);
+                k++;
+            }
+            fscanf(inputfp, "%f", &val_mat[eff_row]);
+            i++;
+            // now we have our A matrix and b matrix in desired form
+        }
+        // now we calculate the disp vector for scatterv
+        ll itr = 1;
+        while (itr < num_proc)
+        {
+            divs[itr - 1] = num_eq * divs[itr - 1];
+            displs[itr] = divs[itr - 1] + displs[itr - 1];
+            itr++;
+        }
+        divs[num_proc - 1] = num_eq * divs[num_proc - 1], fclose(inputfp);
+    }
+
+    
 
     MPI_Finalize();
     return EXIT_SUCCESS;
